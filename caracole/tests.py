12c1f7b936378aa9b893cfd7240b62336ef0b966
Guilhem Saurel <guilhem.saurel@gmail.com>
2016-05-20 17:43:17 +0200

    CI: Sauce

--- caracole/tests.py
+++ caracole/tests.py
@@ -10,19 +10,8 @@ from selenium import webdriver
 from .models import Caracolien
 
 
-
 class CaracolienTests(TestCase):
     def setUp(self):
-        if "TRAVIS" in os.environ:
-            capabilities = {
-                "tunnel-identifier": os.environ["TRAVIS_JOB_NUMBER"],
-                "build": os.environ["TRAVIS_BUILD_NUMBER"],
-                "tags": [os.environ["TRAVIS_PYTHON_VERSION"], "CI"],
-            }
-            hub_url = "%s:%s@localhost:4445/wd/hub" % (os.environ["SAUCE_USERNAME"], os.environ["SAUCE_ACCESS_KEY"])
-            self.browser = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s" % hub_url)
-        else:
-            self.browser = webdriver.Firefox()
         for guy in 'abc':
             User.objects.create_user(guy, email='%s@example.org' % guy, password=guy)
         a, b, c = Caracolien.objects.order_by('pk')
@@ -31,9 +20,6 @@ class CaracolienTests(TestCase):
         b.adhesion = date.today() - timedelta(days=1000)
         a.save()
         b.save()
-
-    def tearDown(self):
-        self.browser.quit()
 
     # MODELS
 
@@ -63,6 +49,26 @@ class CaracolienTests(TestCase):
         self.client.login(username='b', password='b')
         self.client.post(reverse('profil-caracolien'), {'phone_number': '06 424.83 000', 'address': ''})
         self.assertEqual(Caracolien.objects.get(user__username='b').phone_number, '+33642483000')
+
+
+class CaracolienFunctionnalTests(TestCase):
+    def setUp(self):
+        if "TRAVIS" in os.environ:
+            capabilities = {
+                "tunnel-identifier": os.environ["TRAVIS_JOB_NUMBER"],
+                "build": os.environ["TRAVIS_BUILD_NUMBER"],
+                "tags": [os.environ["TRAVIS_PYTHON_VERSION"], "CI"],
+                'browserName': "firefox",
+                'platform': "Linux",
+                'version': "45.0",
+            }
+            hub_url = "%s:%s@localhost:4445/wd/hub" % (os.environ["SAUCE_USERNAME"], os.environ["SAUCE_ACCESS_KEY"])
+            self.browser = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s" % hub_url)
+        else:
+            self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
 
     def test_simple_user(self):
         self.browser.get('http://localhost:8000')
