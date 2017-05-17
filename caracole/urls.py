0f4f818d157495469c683eaf36ba8b329333346a
Guilhem Saurel <guilhem.saurel@gmail.com>
2017-05-17 22:55:48 +0200

    home page is now redirected to videgrenier:home

--- caracole/urls.py
+++ caracole/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 
 from .views import profil, profil_password
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    # url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', RedirectView.as_view(pattern_name='videgrenier:home'), name='home'),
     url(r'^vide-grenier/', include('videgrenier.urls')),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^profil$', profil, name='profil'),
