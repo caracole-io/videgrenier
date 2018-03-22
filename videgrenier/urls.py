45f411cfda65e13598da4a23df20193e259062c6
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-03-22 23:20:30 +0100

    clean urls.py imports

--- videgrenier/urls.py
+++ videgrenier/urls.py
@@ -1,19 +1,18 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import (ReservationDeleteView, ReservationDetailView, ReservationListView,
-                    ReservationModerateView, csview, reservation)
+from . import views
 
 app_name = 'videgrenier'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='videgrenier/home.html'), name='home'),
     path('plan', TemplateView.as_view(template_name='videgrenier/plan.html'), name='plan'),
     path('reglement', TemplateView.as_view(template_name='videgrenier/reglement.html'), name='reglement'),
-    path('reservation', reservation, name='reservation'),
-    path('reservation/detail', ReservationDetailView.as_view(), name='reservation-detail'),
-    path('reservation/annuler', ReservationDeleteView.as_view(), name='reservation-delete'),
-    path('reservation/<int:pk>/<int:accepte>', ReservationModerateView.as_view(), name='reservation-moderate'),
-    path('admin', ReservationListView.as_view(), name='reservation-list'),
-    path('csv', csview, name='csv'),
+    path('reservation', views.reservation, name='reservation'),
+    path('reservation/detail', views.ReservationDetailView.as_view(), name='reservation-detail'),
+    path('reservation/annuler', views.ReservationDeleteView.as_view(), name='reservation-delete'),
+    path('reservation/<int:pk>/<int:accepte>', views.ReservationModerateView.as_view(), name='reservation-moderate'),
+    path('admin', views.ReservationListView.as_view(), name='reservation-list'),
+    path('csv', views.csview, name='csv'),
     path('fini', TemplateView.as_view(template_name='videgrenier/fini.html'), name='fini'),
 ]
