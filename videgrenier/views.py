9037fa7e3ab9ac66019139fd71ff9b0c1f3262f2
Guilhem Saurel <guilhem.saurel@laas.fr>
2018-06-21 13:24:08 +0200

    add missing include

--- videgrenier/views.py
+++ videgrenier/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse, reverse_lazy
-from django.views.generic import DeleteView, DetailView, ListView, UpdateView
+from django.views.generic import DeleteView, DetailView, ListView, TemplateView, UpdateView
 
 from ndh.utils import query_sum
 
