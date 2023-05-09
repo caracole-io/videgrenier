1ab3fe797f3132a9de1ed95bc091a0d22de2fa5d
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-05-09 13:53:40 +0200

    poetry run ruff --fix .

--- videgrenier/views.py
+++ videgrenier/views.py
@@ -169,8 +169,7 @@ def csview(request: HttpRequest) -> HttpResponse:
                 f"{reservation.birthdate} à {reservation.birthplace}",
                 reservation.address,
                 reservation.phone_number,
-                "n°%s delivrée le %s par %s"
-                % (reservation.id_num, reservation.id_date, reservation.id_org),
+                f"n°{reservation.id_num} delivrée le {reservation.id_date} par {reservation.id_org}",
                 reservation.plaque,
                 reservation.emplacements,
                 reservation.nature,
